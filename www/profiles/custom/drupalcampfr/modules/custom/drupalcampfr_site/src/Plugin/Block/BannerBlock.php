900a56ef18b93d2925932b2b7ff6de3fa1910048
Florent Torregrosa <florent.torregrosa@gmail.com>
2016-04-09 14:02:22 +0200

    Add banner block.

---
+++ www/profiles/custom/drupalcampfr/modules/custom/drupalcampfr_site/src/Plugin/Block/BannerBlock.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\drupalcampfr_site\Plugin\Block\BannerBlock.
+ */
+
+namespace Drupal\drupalcampfr_site\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'BannerBlock' block.
+ *
+ * @Block(
+ *  id = "banner_block",
+ *  admin_label = @Translation("Banner block"),
+ * )
+ */
+class BannerBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = array(
+      '#theme' => 'drupalcampfr_site_banner_block',
+    );
+
+    return $build;
+  }
+
+}
