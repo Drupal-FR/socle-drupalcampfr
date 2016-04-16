efc1185e07d5204b2aa2ffb19f78a7253676d34e
Sebastien Corbin <seb.corbin@gmail.com>
2016-04-16 18:14:25 +0200

    Add bootstrap custom build

---
+++ www/themes/custom/nantes2016/gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    LessPluginCleanCSS = require('less-plugin-clean-css'),
+    cleancss = new LessPluginCleanCSS({advanced: true}),
+    less = require('gulp-less'),
+    imagemin = require('gulp-imagemin'),
+    sourcemaps = require('gulp-sourcemaps'),
+    use_sourcemaps = true;
+
+
+// LESS compilation
+gulp.task('less', function () {
+    var pipe = gulp.src('./less/style.less');
+    if (use_sourcemaps) {
+        pipe = pipe.pipe(sourcemaps.init());
+    }
+    pipe = pipe
+        .pipe(less({
+            plugins: [cleancss]
+        }))
+        .pipe(rename({suffix: '.min'}));
+    if (use_sourcemaps) {
+        pipe = pipe.pipe(sourcemaps.write('maps'));
+    }
+    return pipe
+        .pipe(gulp.dest('./assets/css/'))
+        .on('error', errorHandler);
+});
+
+// Optimisation des images
+gulp.task('images', function () {
+    return gulp.src(['*/img*/*'])
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest('.')).on('error', errorHandler);
+});
+
+gulp.task('default', ['less']);
+gulp.task('watch', function () {
+    gulp.watch(['./less/*'], ['less']);
+    gulp.watch(['./svg/*'], ['icons']);
+    gulp.watch(['*/js/*', '../modules/**/*.js'], ['jshint']);
+    gulp.watch(['*/img/*', '!./img/sprite.png'], ['images']);
+});
+
+// Handle the error
+function errorHandler(error) {
+    console.log(error.toString());
+    this.emit('end');
+}
