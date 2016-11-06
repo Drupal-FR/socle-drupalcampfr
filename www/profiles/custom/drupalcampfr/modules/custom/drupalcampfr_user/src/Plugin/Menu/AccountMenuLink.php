dc87ade2fb76071253b575463a7cdae99cef1eb0
Florent Torregrosa <florent.torregrosa@gmail.com>
2016-11-06 14:51:09 +0100

    Fix user menu not displayed for authenticated users.

---
+++ www/profiles/custom/drupalcampfr/modules/custom/drupalcampfr_user/src/Plugin/Menu/AccountMenuLink.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\drupalcampfr_user\Plugin\Menu;
+
+/**
+ * A menu link that shows "Account" to the appropriated path.
+ */
+class AccountMenuLink extends MyAccountCreateAccountMenuLink {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle() {
+    return $this->t('Account');
+  }
+
+}
