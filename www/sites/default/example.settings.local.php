3f82b4750630e52b4b0ba66d91e3e38242a7dec4
Florent Torregrosa <florent.torregrosa@gmail.com>
2017-01-28 19:57:22 +0100

    Put cache config into setting.php instead of settings.local.php

--- www/sites/default/example.settings.local.php
+++ www/sites/default/example.settings.local.php
@@ -15,19 +15,6 @@ $settings['trusted_host_patterns'] = array(
   '^127\.0\.0\.1$',
 );
 
-// External cache.
-if (file_exists(__DIR__ . '/.cache_activated')) {
-  $settings['redis.connection']['interface'] = 'PhpRedis';
-  $settings['redis.connection']['host']      = 'redis';
-  $settings['cache']['default'] = 'cache.backend.redis';
-
-  // Always set the fast backend for bootstrap, discover and config, otherwise
-  // this gets lost when redis is enabled.
-  $settings['cache']['bins']['bootstrap'] = 'cache.backend.chainedfast';
-  $settings['cache']['bins']['discovery'] = 'cache.backend.chainedfast';
-  $settings['cache']['bins']['config'] = 'cache.backend.chainedfast';
-}
-
 // Twitter.
 // Go to https://apps.twitter.com to get these tokens.
 $config['drupalcampfr_social.twitter']['consumer_key'] = '';
@@ -36,7 +23,7 @@ $config['drupalcampfr_social.twitter']['access_token'] = '';
 $config['drupalcampfr_social.twitter']['access_token_secret'] = '';
 
 // Contact email.
-$contact_email = '';
+$contact_email = 'example@example.org';
 $config['contact.form.attendance_certificate']['recipients'] = array($contact_email);
 $config['contact.form.contact']['recipients']                = array($contact_email);
 $config['contact.form.demander_une_reduction']['recipients'] = array($contact_email);
