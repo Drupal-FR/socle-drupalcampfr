6d038d5189236bd9a3314d0cf4bf6a964471d3e4
Florent Torregrosa <florent.torregrosa@gmail.com>
2017-04-29 12:49:54 +0200

    Use short array syntax.

--- www/sites/default/example.settings.local.php
+++ www/sites/default/example.settings.local.php
@@ -1,20 +1,20 @@
 <?php
 
-$databases['default']['default'] = array(
+$databases['default']['default'] = [
   'driver' => 'mysql',
   'database' => 'drupal',
   'username' => 'drupal',
   'password' => 'drupal',
   'host' => 'mysql',
   'prefix' => '',
-);
+];
 
 $settings['hash_salt'] = 'drupalcampfr';
-$settings['trusted_host_patterns'] = array(
+$settings['trusted_host_patterns'] = [
   '^127\.0\.0\.1$',
   'varnish',
   'web',
-);
+];
 
 // Redis.
 $settings['redis.connection']['host'] = 'redis';
@@ -31,11 +31,12 @@ $config['drupalcampfr_social.twitter']['access_token_secret'] = '';
 
 // Contact email.
 $contact_email = 'example@example.org';
-$config['contact.form.attendance_certificate']['recipients'] = array($contact_email);
-$config['contact.form.contact']['recipients']                = array($contact_email);
-$config['contact.form.demander_une_reduction']['recipients'] = array($contact_email);
-$config['contact.form.sponsor']['recipients']                = array($contact_email);
-$config['contact.form.volunteer']['recipients']              = array($contact_email);
+
+$config['contact.form.attendance_certificate']['recipients'] = [$contact_email];
+$config['contact.form.contact']['recipients']                = [$contact_email];
+$config['contact.form.demander_une_reduction']['recipients'] = [$contact_email];
+$config['contact.form.sponsor']['recipients']                = [$contact_email];
+$config['contact.form.volunteer']['recipients']              = [$contact_email];
 
 if (file_exists(__DIR__ . '/../development.settings.php')) {
   include __DIR__ . '/../development.settings.php';
