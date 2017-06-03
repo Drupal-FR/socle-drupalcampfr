414e8cb2a20c466d48bbfab99645a3efdfb99322
Anne-Sophie PICOT <aspicot@actualys.com>
2017-06-03 09:57:06 +0200

    commit initial theme bzh

---
+++ www/profiles/drupalcampfr/themes/bzh_theme/src/Plugin/Preprocess/MenuAccount.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\bzh_theme\Plugin\Preprocess;
+
+use Drupal\bootstrap\Plugin\Preprocess\PreprocessBase;
+use Drupal\bootstrap\Utility\Variables;
+use Drupal\Component\Render\FormattableMarkup;
+
+/**
+ * Implements hook_preprocess_menu__account().
+ *
+ * @ingroup plugins_preprocess
+ *
+ * @BootstrapPreprocess("menu__account")
+ */
+class MenuAccount extends PreprocessBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preprocessVariables(Variables $variables) {
+    // Add icon on user link.
+    if (isset($variables['items']['drupalcampfr_user.account.compte'])) {
+      $variables['items']['drupalcampfr_user.account.compte']['title'] = new FormattableMarkup(
+        '<span class="glyphicon glyphicon-user" aria-hidden="true"></span> @original_title',
+        [
+          '@original_title' => $variables['items']['drupalcampfr_user.account.compte']['title'],
+        ]
+      );
+    }
+
+    parent::preprocessVariables($variables);
+  }
+
+}
