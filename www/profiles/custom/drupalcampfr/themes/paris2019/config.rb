5944c781084c91df34f248f376eaa8ed30eee016
Kevin Gautreau <contact@kgaut.net>
2018-11-15 09:30:06 +0100

    scaffolding integration dcp2019

---
+++ www/profiles/custom/drupalcampfr/themes/paris2019/config.rb
@@ -0,0 +1,25 @@
+# Change this to :production when ready to deploy the CSS to the live server.
+#environment = :development
+environment = :production
+
+# Location of the theme's resources.
+css_dir         = "assets/css"
+sass_dir        = "assets/sass"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = (environment == :development) ? :expanded : :compressed
+
+# To enable relative paths to assets via compass helper functions. Since Drupal
+# themes can be installed in multiple locations, we don't need to worry about
+# the absolute path to the theme from the server root.
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+# Pass options to sass. For development, we turn on the FireSass-compatible
+# debug_info if the firesass config variable above is true.
+sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
+
+sourcemap = (environment == :development) ? true : false;
